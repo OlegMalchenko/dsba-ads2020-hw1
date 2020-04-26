8838767fe0ca96e010e6f6c36e0c023c2a5e8d90
Oleg <55272140+OlegMalchenko@users.noreply.github.com>
2020-04-26 18:33:55 +0300

    Add files via upload

---
+++ multiplicator_graph_drawer.py
@@ -0,0 +1,20 @@
+
+import matplotlib.pyplot as pplt
+from matplotlib import style
+import pandas as csv
+
+figure = pplt.figure()
+
+graph_data = csv.read_csv("J:\\MyFiles\\graph_data_new.csv", delimiter = ";", names = ['problem size', 'GSM', 'KBM', 'DNC'])
+
+pplt.plot(graph_data['problem size'], graph_data['GSM'], color = 'orange', label = 'Grade School Algo.')
+pplt.plot(graph_data['problem size'], graph_data['KBM'], color = 'red', label = 'Karatsuba Algo.')
+pplt.plot(graph_data['problem size'], graph_data['DNC'], color = 'blue', label = 'Divide & Conquer Algo. ')
+
+style.use('seaborn-talk')
+pplt.title("ADS Big Homework 1, Algorithms' complexity comparison")
+pplt.ylabel('Time (in seconds)')
+pplt.xlabel('Problem size (in num. of digits)')
+
+pplt.legend()
+pplt.show()
